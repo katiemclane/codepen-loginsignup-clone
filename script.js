378ab11d89b78b2589d3463d919f5cb8e0ccddc0
katiemclane <katiemclane@me.com>
2014-01-25 13:55:37 -0700

    Don't you understand?

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-/*
+$('document').ready(function() {
+	$('.signup-form').hide();
+	$( ".signup-form-btn" ).click(function() {
+		$('.signup-form').show()
+ 		$('.login-form').hide()
+ 		$('.signup-form-btn').addClass('active')
+ 		$('.login-form-btn').removeClass('active')
 
-############## API ##############
+	});
+	$( ".login-form-btn" ).click(function() {
+		$('.signup-form').hide()
+ 		$('.login-form').show()
+ 		$('.signup-form-btn').removeClass('active')
+ 		$('.login-form-btn').addClass('active')
+
+	});
+	//listen for click on login
+	$('.btn-login').click(function(){
+		var user = Object.create(NewUser)
+		loginUser.username = $('#login-username-field').val()
+		loginUser.password = $('#login-password-field').val()
+		var response=codepen.api.login(user)
+		console.log(response)
+	//instantiate an object wirh username and pass
+	
+
+	//call the login method and passit our previously instantiated object and handle the response
+	});
+});
+
+/*############## API ##############
 
 codepen.api.signup(user_object)
 	description: to sign up a new user
@@ -25,12 +53,4 @@ Reponse Objects:
 	('suzy', 'I@msoawesome')
 	('conan', 'gingertastic')
 	('jerry', '@#!$%@')
-
-*/
-
-
-$('document').ready(function() {
-
-	//put code here
-
-});
+	*/
